4250f6c789917ad526f69d877eb6a1ebe6f6b927
JohnsonNYC <jkow95@gmail.com>
2025-08-12 22:29:12 -0400

    comment out unused import

--- app/components/TattooGallery.tsx
+++ app/components/TattooGallery.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { motion } from 'framer-motion';
+// import { motion } from 'framer-motion';
 
 const tattoos = [
   { id: 1, src: '/images/tattoo1.jpg', alt: 'Tattoo 1' },
